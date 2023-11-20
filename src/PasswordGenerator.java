4c0a2b8830de3a41e44efe02fb0ed55d26d27ecb
Cesar Santos <cesarsm96@outlook.com>
2023-11-20 15:55:01 -0500

    upgrade

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -10,7 +10,33 @@ public class PasswordGenerator {
     public PasswordGenerator(int lengthPassword) {
         this.lengthPassword = lengthPassword;
         this.utility = utility;
+import java.security.SecureRandom;
 
+        public class PasswordGenerator {
+            private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=_+";
+            private static final SecureRandom random = new SecureRandom();
+
+            private int lengthPassword;
+            private  String utility;
+
+            public PasswordGenerator(int lengthPassword) {
+                this.lengthPassword = lengthPassword;
+                this.utility = utility;
+
+            }
+
+            public String generatePassword() {
+                StringBuilder senha = new StringBuilder();
+                for (int i = 0; i < lengthPassword; ++i) {
+                    int indice = random.nextInt(CHARACTERS.length());
+                    senha.append(CHARACTERS.charAt(indice));
+                }
+                return senha.toString();
+            }
+            public String getUtility(){
+                return utility;
+            }
+        }
     }
 
     public String generatePassword() {
